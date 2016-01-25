e474d4dac23a43cd6d88e4328f82020963c6f4c7
Marek Serafin <m.serafin@kontakt.io>
2016-01-25 10:44:20 +0100

    Bug fixes

--- KontaktSDK/KontaktSDK.framework/Headers/KTKEddystoneFrame.h
+++ KontaktSDK/KontaktSDK.framework/Headers/KTKEddystoneFrame.h
@@ -1,6 +1,6 @@
 //
 //  KontaktSDK
-//  Version: 1.0.0
+//  Version: 1.0.1
 //
 //  Copyright (c) 2015 Kontakt.io. All rights reserved.
 //
--- KontaktSDK/KontaktSDK.framework/Headers/KTKVenue.h
+++ KontaktSDK/KontaktSDK.framework/Headers/KTKVenue.h
@@ -1,6 +1,6 @@
 //
 //  KontaktSDK
-//  Version: 1.0.0
+//  Version: 1.0.1
 //
 //  Copyright © 2015 Kontakt.io. All rights reserved.
 //
