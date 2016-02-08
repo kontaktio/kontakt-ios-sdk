230eb92ac17a9c332c27535b60614ab2924fa2a5
Marek Serafin <m.serafin@kontakt.io>
2016-02-08 16:49:55 +0100

    Fixing Bitcode Symbols

--- KontaktSDK/KontaktSDK.framework/Headers/KTKActionContent.h
+++ KontaktSDK/KontaktSDK.framework/Headers/KTKActionContent.h
@@ -1,6 +1,6 @@
 //
 //  KontaktSDK
-//  Version: 1.0.3
+//  Version: 1.0.4
 //
 //  Copyright © 2015 Kontakt.io. All rights reserved.
 //
--- KontaktSDK/KontaktSDK.framework/Headers/KontaktSDK.h
+++ KontaktSDK/KontaktSDK.framework/Headers/KontaktSDK.h
@@ -1,6 +1,6 @@
 //
 //  KontaktSDK
-//  Version: 1.0.3
+//  Version: 1.0.4
 //
 //  Copyright (c) 2015 Kontakt.io. All rights reserved.
 //
