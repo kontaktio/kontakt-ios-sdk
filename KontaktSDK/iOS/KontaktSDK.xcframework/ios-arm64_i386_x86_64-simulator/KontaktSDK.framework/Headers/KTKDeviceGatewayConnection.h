8a864aba3df9622a4fdd47396b0aedb74b3ce5ef
Jack <>
2022-04-12 15:22:52 +0200

    - Added support for new devices - Added support for XCFramework

---
+++ KontaktSDK/iOS/KontaktSDK.xcframework/ios-arm64_i386_x86_64-simulator/KontaktSDK.framework/Headers/KTKDeviceGatewayConnection.h
@@ -0,0 +1,52 @@
+//
+//  KontaktSDK
+//  Version: 3.0.22
+//
+//  Copyright © 2017 Kontakt.io. All rights reserved.
+//
+
+#import "KTKDeviceConnection.h"
+#import "KTKDeviceGatewayWiFiNetwork.h"
+#import "KTKDeviceGatewayConfiguration.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark - Completion Blocks
+
+/**
+ *  A completion block object to be executed when the reading WiFi networks operation finishes.
+ *
+ *  @param networks Set of WiFi networks visible by the Gateway.
+ *  @param configuration The configuration object. Result of the read operation.
+ *  @param error         An error object containing the error that indicates why the operation failed.
+ */
+typedef void (^KTKDeviceGatewayConnectionGetWiFiBlock)(NSSet <KTKDeviceGatewayWiFiNetwork *>* _Nullable networks, __kindof KTKDeviceGatewayConfiguration * _Nullable configuration, NSError * _Nullable error);
+typedef void (^KTKDeviceGatewayConnectionGetMACBlock)(NSString * _Nullable mac, NSError * _Nullable error);
+
+#pragma mark - KTKDeviceGatewayConnection (Interface)
+@interface KTKDeviceGatewayConnection : KTKDeviceConnection
+
+#pragma mark - Connection Methods
+///--------------------------------------------------------------------
+/// @name Connection Methods
+///--------------------------------------------------------------------
+
+- (void)writeUsingCloudConfiguration:(KTKDeviceConfiguration *)configuration completion:(KTKDeviceConnectionWriteCompletion)completion;
+
+/**
+ *  Reads the configuration from the connection device.
+ *
+ *  @param completion A block object to be executed when the read operation finishes.
+ */
+- (void)getWiFiNetworks:(KTKDeviceGatewayConnectionGetWiFiBlock)completion;
+
+/**
+ *  Reads the configuration from the connection device using MAC.
+ *
+ *  @param completion A block object to be executed when the read operation finishes.
+ */
+- (void)getMacDevice:(KTKDeviceGatewayConnectionGetMACBlock)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
